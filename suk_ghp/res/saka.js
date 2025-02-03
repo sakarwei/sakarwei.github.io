249a2ed74d5d832b1faf8c379aa70c14e21d6e03
Jeun <9508988+jeun1358@user.noreply.gitee.com>
2025-02-04 04:22:04 +0800

    250204

--- suk_ghp/res/saka.js
+++ suk_ghp/res/saka.js
@@ -2,6 +2,6 @@ function $(s){return document.querySelector(s)}
 function $$(s){return document.querySelectorAll(s)}
 
 function fold(s){
-	if($$("oi-veki")[s-1].style.display!="block"){$$("oi-veki")[s-1].style.display="block";$$("a.oi-veki")[s-1].innerText="折起";}
-	else{$$("oi-veki")[s-1].style.display="";$$("a.oi-veki")[s-1].innerText="展开";}
+	if($$("oi-veki")[s-1].style.display!="block"){$$("oi-veki")[s-1].style.display="block";$$("a.oi-veki")[s-1].innerText=$$("a.oi-veki")[s-1].getAttribute("close")||"折起";}
+	else{$$("oi-veki")[s-1].style.display="";$$("a.oi-veki")[s-1].innerText=$$("a.oi-veki")[s-1].getAttribute("open")||"展开";}
 }
